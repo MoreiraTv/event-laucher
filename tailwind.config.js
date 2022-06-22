03c2583c5a7e3b7d67229e5c7602fb12d497ccd0
Matheus Moreira <matheusmoreira08@gmail.com>
2022-06-22 01:18:59 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './scr/**/*.{html,tsx, ts, js}',
+    "./src/**/*"
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
